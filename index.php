4937df1596f78a92e8ee60cc4586618660992202
ragav <ragavendran@kutung.in>
2016-08-02 13:24:26 +0530

    changes in index file

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
 
-echo "BM";
+include('config.php');
+
+
+$mysql = mysql_query("select * from bm_admin_login_user");
+while($results = mysql_fetch_assoc($mysql))
+{
+    print_r($results);
+}
+
+
 
 
 
